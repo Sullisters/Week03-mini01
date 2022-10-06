b910c04595345253dc8ee2da03f585b5fea152f6
Sullisters <heatonpschreiber@gmail.com>
2022-10-06 19:31:55 -0400

    finalized project

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,29 @@
 var timeDisplay = $('#currentDay');
 var timeEl = ('.hour');
-var saveBtn = $('.saveBtn')
-var centerRowEl = $('.textarea')
+var saveBtn = $('.saveBtn');
+var centerRowEl = $('.textarea');
+var eventInput = $('.fname');
+var btnIcon = $('.fa-save')
 
 // Clock display on jumbotron
 function displayTime(){
     var now = moment().format('ddd, MMMM Do hh:mm:ss')
     timeDisplay.text(now)
 }
-
-//Function that handles where the current time is at on the table
-function setTimeStyles() {
-    var currentTime = new Date().getHours();
-    if(timeEl < now) {
-
-        //Not sure if these are correct    
-        centerRowEl.addclass('past');
-    }
-    else if (timeEl > now) {
-        centerRowEl.addclass('future');
-    } else {
-        centerRowEl.addClass('present');
-    }
-}
-
 // The second counter, in milliseconds
 setInterval(displayTime, 1000);
+
+// If statement for checking current time and setting classes based on if it is before/afer/current
+var objDate = moment().format();
+    if(objDate === timeDisplay) {
+        $(centerRowEl).addClass('present');
+    } else if(objDate < timeDisplay) {
+        $(centerRowEl).addClass('past');
+    } else {
+        $(centerRowEl).addClass('future');
+    }
+
+let eventStorage = localStorage.getItem('centerRowEl')
+    ? JSON.parse(localStorage.getItem('centerRowEl'))
+    : [];
+
